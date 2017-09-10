010f2968a6c6033c37c5ec2a7c68677c9bc78300
digitopoi <hubo85@gmail.com>
2017-09-10 16:56:55 -0500

    Remove uneccessary price-display files

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -12,8 +12,7 @@ import {
   selector: 'price-display',
   template: `
     <div class="price-display">\${{ price }}</div>
-  `,
-  styleUrls: ['./price-display.component.css']
+  `
 })
 export class PriceDisplayComponent {
   @Input() price: number;
