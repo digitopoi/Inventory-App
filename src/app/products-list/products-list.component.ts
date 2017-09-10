1610edb4845b22617a8231ed01eb98d7ca15b554
digitopoi <hubo85@gmail.com>
2017-09-10 15:39:34 -0500

    Update ProductListComponent controller with currentProduct property and onProducts selected EventEmitter

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,13 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../product.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
+/**
+ *  @ProductsList: A component for rendering all ProductRows and
+ *  storing the currently selected Product
+ */
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  // tslint:disable-next-line:component-selector
+  selector: 'products-list',
+  templateUrl: './products-list.component.html'
 })
 export class ProductsListComponent implements OnInit {
+  /**
+   *  @input productList - the Product[] passed to us
+   */
+  @Input() productList: Product[];
 
-  constructor() { }
+  /**
+   *  @output onProductSelected - outputs the current
+   *          Product whenever a new Product is selected
+   */
+  @Output() onProductSelected: EventEmitter<Product>;
+
+  /**
+   *  @property currentProduct - local state containing
+   *            the currently selected 'Product'
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
 
   ngOnInit() {
   }
