d6ec6ee372f279b8c6b18cfd6cc705b282ae99fb
digitopoi <hubo85@gmail.com>
2017-09-10 14:05:27 -0500

    update main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  // tslint:disable-next-line:component-selector
+  selector: 'inventory-app-root',
+  template: `
+    <div class="inventory-app">
+      (Products will go here soon)
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //  Inventory logic here
 }
