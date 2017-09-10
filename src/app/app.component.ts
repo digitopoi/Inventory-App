dd8aa65549739cc1abaa2c675a24c158baefe603
digitopoi <hubo85@gmail.com>
2017-09-10 16:55:41 -0500

    change app component from implicit template to html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'inventory-app-root',
-  template: `
-    <div class="inventory-app">
-      (Products will go here soon)
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
